ded17c038541b0127cd00cee18687e757cfab94a
Ashish <ashishkeshri3016@gmail.com>
2023-02-16 00:23:50 +0530

    push entered contacts into contact list

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded());
+
 var contactList = [
     {
         name: 'Ashish',
@@ -33,6 +35,17 @@ app.get('/practice', function(req, res){
     return res.render('practice',{
         title: 'play with ejs'
     });
+});
+
+app.post('/create-contact', function(req, res){
+    // contactList.push({
+    //     name: req.body.name,
+    //     phone: req.body.phone
+    // });
+
+    contactList.push(req.body);
+
+    return res.redirect('/');
 });
 
 app.listen(port, function(err){
